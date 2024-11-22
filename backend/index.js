8d33e396c63742a81eee5eff5a9b3b83e2dc22d1
anil gummula <anilgummula2060@gmail.com>
2024-11-22 23:31:19 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ require('./models/db.js');
 // const UserModel = require('./models/user.js');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 // app.use(express.json());
 app.use(bodyParser.json());
