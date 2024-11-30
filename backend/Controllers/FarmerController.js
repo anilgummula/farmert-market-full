6cb1707c5d1b973a8a1c803aae4e06f6e9d0d194
anil gummula <anilgummula2060@gmail.com>
2024-12-01 01:47:03 +0530

    updated

--- backend/Controllers/FarmerController.js
+++ backend/Controllers/FarmerController.js
@@ -1,12 +1,14 @@
-// const User = require('../models/user');
+const User = require('../models/user');
 const Product = require('../models/Product');
 const Order = require('../models/Order'); // Ensure Order model is required if used in manageOrder
 
 // Fetch products listed by the farmer
 exports.getMyProducts = async (req, res) => {
   try {
-    const products = await Product.find({ farmer: req.user._id });
-    res.status(200).json({ success: true, products });
+    const products = await Product.find({ farmer : req.user._id });
+    // const product = await Product.findOne({farmer : req.user._id});
+    const farmerDetails =await User.findById(req.user._id);
+    res.status(200).json({ success: true, products ,farmerDetails : farmerDetails});
   } catch (err) {
     res.status(500).json({ success: false, message: 'Failed to fetch products!', error: err.message });
   }
@@ -64,10 +66,19 @@ exports.manageOrder = async (req, res) => {
         // const user = await user.findById(buyerId);
         const product = await Product.findById(productId);
         product.quantity += orderQunatity;
-        product.save();
+        await product.save();
+    }
+    if(status === "Accepted"){
+        const productId = order.product;
+        const product = await Product.findById(productId);
+        const orderQunatity = order.quantity;
+        const earnings = (orderQunatity) * (product.price);
+        const user = await User.findById(order.farmer);
+        user.myEarnings += earnings; 
+        await user.save();
     }
 
-    if (!order) return res.status(404).json({ success: false, message: 'Order not found!' });
+    if (!order) return res.status(404).json({ success: false, message: 'Order not found!'});
 
     order.status = status;
     await order.save();
--- backend/models/user.js
+++ backend/models/user.js
@@ -5,6 +5,7 @@ const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   type: { type: String, enum: ['Farmer', 'Retailer'], required: true }, // Farmer or Retailer
+  myEarnings : {type: Number, default :0 }
 });
 
 module.exports = mongoose.model('user', UserSchema);
